cc5173a810b4ec471abb309661d23ab4733d4e98
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-09 15:34:15 +1000

    add create user function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -10,3 +10,25 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const CERT_PATH = path.join(__dirname, "../certs/aws-rds-global-bundle.pem");
 
+
+// Create new user
+const createUser = (request, response) => {
+  const { name, email, password } = request.body;
+  // source: https://heynode.com/blog/2020-04/salt-and-hash-passwords-bcrypt/ - salting and hashing at the same time
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      throw err;
+    }
+    pool.query(
+      "INSERT INTO users (first_name, email, password_hash) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, hash],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+      }
+    );
+  });
+};
+
