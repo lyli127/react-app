750eca47fd0b16d4100701771c30cbf2e8d56a17
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-09 15:35:07 +1000

    add  delete user account

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -65,3 +65,15 @@ const updateUser = (request, response) => {
   );
 };
 
+//Delete user account
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User account deleted with ID: ${id}`);
+  });
+};
+
