8f601ce394abe10f76029610d628daafc724471e
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-09 15:30:56 +1000

    add create ramen review

--- controllers/ramenController.js
+++ controllers/ramenController.js
@@ -7,3 +7,55 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+//Create new ramen review
+const createRamenReview = (request, response) => {
+  const {
+    restaurant_name,
+    dish_name,
+    photo_url,
+    ramen_type,
+    ramen_score,
+    noodle_texture,
+    noodle_score,
+    broth_type,
+    broth_score,
+    chashu_type,
+    chashu_score,
+    ajitama,
+    ajitama_score,
+    other_notes,
+    date_visited,
+    user_id,
+  } = request.body;
+  pool.query(
+    "INSERT INTO ramen (restaurant_name, dish_name, photo_url, ramen_type,ramen_score, noodle_texture, noodle_score, broth_type, broth_score, chashu_type, chashu_score, ajitama, ajitama_score, other_notes, date_visited, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING *",
+    [
+      restaurant_name,
+      dish_name,
+      photo_url,
+      ramen_type,
+      ramen_score,
+      noodle_texture,
+      noodle_score,
+      broth_type,
+      broth_score,
+      chashu_type,
+      chashu_score,
+      ajitama,
+      ajitama_score,
+      other_notes,
+      date_visited,
+      user_id,
+    ],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response
+        .status(201)
+        .send(`Ramen review added with ID: ${results.rows[0].id}`);
+    }
+  );
+};
+
