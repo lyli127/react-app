8eebe3268cd512d96c086a6a61255b2e63d399c9
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-09 15:34:33 +1000

    add get user by id

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -32,3 +32,14 @@ const createUser = (request, response) => {
   });
 };
 
+//Get user by id
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
