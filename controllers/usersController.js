34bd6dcfaddfbb7bd2968381c9cfc1fc981dd916
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-09 15:34:49 +1000

    add update user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -43,3 +43,25 @@ const getUserById = (request, response) => {
     response.status(200).json(results.rows);
   });
 };
+
+// Update user
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { name, email } = request.body;
+
+  pool.query(
+    "UPDATE users SET first_name = $1, email = $2 WHERE id = $3",
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response
+        .status(200)
+        .send(
+          `User modified with ID: ${id}, Name: {first_name}, Email: {email}`
+        );
+    }
+  );
+};
+
