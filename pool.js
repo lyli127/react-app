0c50dd59d55479d71967d5b600a87cf19c914392
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-14 10:25:41 +1000

    add pool file

---
+++ pool.js
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import PG from "pg";
+
+// const pool = new PG.Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+// });
+
+import path from "node:path";
+import fs from "node:fs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const CERT_PATH = path.join(__dirname, "../certs/aws-rds-global-bundle.pem");
+
+const pool = new PG.Pool({
+  ssl: {
+    rejectUnauthorized: true,
+    // https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+    ca: fs.readFileSync(CERT_PATH),
+  },
+});
