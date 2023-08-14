74e51ab12f3350e32f7cb7f0eff8b79e421bfed3
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-14 16:03:37 +1000

    update user table

--- models/usersSeedData.sql
+++ models/usersSeedData.sql
@@ -3,11 +3,11 @@ CREATE TABLE users(
       id SERIAL PRIMARY KEY,
       first_name TEXT NOT NULL,
       email TEXT NOT NULL UNIQUE,
-      password_hash TEXT NOT NULL,
-      ramen_type_pref TEXT NOT NULL,
-      noodle_text_pref TEXT NOT NULL,
-      broth_type_pref TEXT NOT NULL,
-      avatar_url TEXT NOT NULL
+      password_hash TEXT,
+      ramen_type_pref TEXT,
+      noodle_text_pref TEXT,
+      broth_type_pref TEXT,
+      avatar_url TEXT 
 );
 INSERT INTO users(first_name, email, password_hash, ramen_type_pref, noodle_text_pref, broth_type_pref, avatar_url)
 VALUES('Test1', 'test@example.com', 'password123', 'Tonkotsu', 'Firm', 'Pork', 'https://i.imgur.com/1XZVYVw.jpg');
