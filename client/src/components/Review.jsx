5a4c00ba04c569897c1323f7bbad768d093ab7ea
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-14 10:23:13 +1000

    add review card component

---
+++ client/src/components/Review.jsx
@@ -0,0 +1,63 @@
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+
+export function Review({ data, children }) {
+  const dateSettings = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return (
+    <Card
+      style={{ width: "18rem" }}
+      // key="Dark"
+      // bg="dark"
+      // text="white"
+      // variant="dark"
+      // border="light"
+      className="mb-2"
+    >
+      <Card.Img
+        variant="top"
+        src={
+          data.photo_url
+            ? data.photo_url
+            : `https://placehold.co/180x100/grey/FFF?font=playfair-display&text=${data.restaurant_name}`
+        }
+      />
+      <Card.Body key={data.id}>
+        <Card.Title>{data.restaurant_name}</Card.Title>
+        <Card.Text>{data.other_notes}</Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Dish: {data.dish_name}</ListGroup.Item>
+        <ListGroup.Item>
+          Type: {data.ramen_type} Score: {data.ramen_score}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          Noodle Texture: {data.noodle_texture} Score: {data.noodle_score}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          Broth: {data.broth_type} Score: {data.broth_score}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          Chashu: {data.chashu_type} Score: {data.chashu_score}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          Ajitama: {data.ajitama} Score: {data.ajitama_score}
+        </ListGroup.Item>
+        <Card.Footer>
+          <small className="text-muted">
+            Visited on{" "}
+            {new Date(data.date_visited).toLocaleString(
+              undefined,
+              dateSettings
+            )}
+          </small>
+        </Card.Footer>
+      </ListGroup>
+      {children}
+    </Card>
+  );
+}
