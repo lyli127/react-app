ea699991e9a31eab97ca0991f0f13fcea3220c68
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-14 10:32:35 +1000

    add import modules to view all reviews page

---
+++ client/src/components/ViewAllReviewsPage.jsx
@@ -0,0 +1,11 @@
+// import ViewReview from "./ViewReview";
+import { Review } from "./Review";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { MainNav } from "./MainNav";
+import { Footer } from "./Footer";
+
+import Button from "react-bootstrap/Button";
+import CardGroup from "react-bootstrap/CardGroup";
+import Spinner from "react-bootstrap/Spinner";
