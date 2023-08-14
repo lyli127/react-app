2ff144dd1a2fae0e763f5df7d6a9cba7f09a08c3
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-14 22:01:46 +1000

    refactor main nav

--- client/src/components/MainNav.jsx
+++ client/src/components/MainNav.jsx
@@ -1,60 +1,47 @@
 //React
 import { React, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 //Bootstrap
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
 //My Components
 import { AccountContext } from "./AccountContext";
 
 export function MainNav(props) {
-  const { setUser } = useContext(AccountContext);
-  const navigate = useNavigate();
+  const { user, setUser } = useContext(AccountContext);
+
   return (
     <>
       <Navbar bg="dark" variant="dark" className="justify-content-center">
         <Nav activeKey="/home">
-          <Navbar.Brand href="#home">
+          <Navbar.Brand href="/home">
             <h1>NOODLE||NEXUS</h1>
             <h1 className="title">ラーメン</h1>
           </Navbar.Brand>
         </Nav>
       </Navbar>
       <Navbar bg="dark" variant="dark" className="justify-content-center">
-        <Nav activeKey="/home">
-          <Nav.Item>
-            <Nav.Link eventKey="link-1">Find Ramen</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="my-reviews">Reviews</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-2">Sign Up</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-2" onClick={props.handleAuthClick}>
-              Logged In? ({props.isLoggedIn ? "Yes" : "No"})
-            </Nav.Link>
-            {/* {user ? (
-              <>
-                <Nav.Item>
-                  <Nav.Link eventKey="">Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="">Sign Up</Nav.Link>
-                </Nav.Item>
-              </>
-            ) : (
-              <Nav.Item>
-                <Nav.Link eventKey="logout" onClick={handleLogout}>
-                  Logout
-                </Nav.Link>
-              </Nav.Item>
-            )} */}
-          </Nav.Item>
-        </Nav>
+        <Container>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-evenly">
+            <Navbar.Text>
+              <Link to={`/find-ramen`}>Find Ramen</Link>
+            </Navbar.Text>
+            <Navbar.Text>
+              <Link to={`/my-reviews`}>Reviews</Link>
+            </Navbar.Text>
+            <Navbar.Text>
+              <Link to={`/signup`}>Sign Up</Link>
+            </Navbar.Text>
+            <Navbar.Text>
+              <Link to={`/login`}>Login ({`${user.loggedIn}`})</Link>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
+      <br />
     </>
   );
 }
