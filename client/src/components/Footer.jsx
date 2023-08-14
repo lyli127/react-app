713eab27f571e73535bb6fd2953366f84e635ee1
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-14 10:31:45 +1000

    add footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+export function Footer() {
+  return (
+    <footer
+      className="footer"
+      style={{ "text-align": "center", "background-image": "../assets/05.svg" }}
+    >
+      <hr style={{ "margin-top": "10%" }} />
+      <div className="container">
+        <span className="text-muted">
+          <br />
+          <img src="./favicon.ico" alt="ramen logo" width="100" />
+          <br />
+          <p>
+            <span className="text-muted">Lylibete Tennikoff © 2021</span>
+          </p>
+          <img src="../assets/linkedin.png" alt="linkedin" />
+          <img src="ramen-app/client/src/assets/github.png" alt="github" />
+          <img src="../assets/email" alt="email" />
+        </span>
+      </div>
+    </footer>
+  );
+}
