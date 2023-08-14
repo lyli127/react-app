2817ab90e1253a668f1693f9b46fe459332356cc
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-14 10:29:46 +1000

    add auth provider func

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,3 +10,25 @@ import ViewAllReviewsPage from "./components/ViewAllReviewsPage";
 import EditReviewPage from "./components/EditReviewPage";
 import FindRamen from "./components/FindRamen";
 import ViewRestaurantReviews from "./components/ViewRestaurantReviews";
+const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  const [user, setUser] = useLocalStorage("user", null);
+
+  const navigate = useNavigate();
+  //call this function when you want to authenticate the user
+  async function login(data) {
+    setUser(data);
+    navigate("/");
+  }
+  //call this function to sign out logged in user
+  function logout() {
+    setUser(null);
+    navigate("/", { replace: true });
+  }
+  //memoization
+  const value = useMemo(() => {
+    user, login, logout;
+  }, [user]);
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
