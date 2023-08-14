908aad53c742fdc84e9d3bfac6cb2aa96e690dee
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-14 22:00:31 +1000

    update account context

--- client/src/components/AccountContext.jsx
+++ client/src/components/AccountContext.jsx
@@ -1,9 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AccountContext = createContext();
 
 const UserContext = ({ children }) => {
   const [user, setUser] = useState({ loggedIn: null });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/login", {
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUser({ loggedIn: data.loggedIn });
+        navigate("/my-reviews");
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log("No user logged in");
+        setUser({ loggedIn: false });
+        // navigate("/");
+      });
+  }, []);
+
   return (
     <AccountContext.Provider value={{ user, setUser }}>
       {children}
