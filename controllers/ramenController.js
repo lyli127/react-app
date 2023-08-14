0c80bba3faba21b2b163b709a9ce12e0dd275c32
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-14 10:15:28 +1000

    add pool

--- controllers/ramenController.js
+++ controllers/ramenController.js
@@ -7,6 +7,15 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const CERT_PATH = path.join(__dirname, "../certs/aws-rds-global-bundle.pem");
+
+const pool = new PG.Pool({
+  ssl: {
+    rejectUnauthorized: true,
+    // https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+    ca: fs.readFileSync(CERT_PATH),
+  },
+});
 
 //Create new ramen review
 const createRamenReview = (request, response) => {
