6c527e14f569b38452585a3069c7f5a5b90da34e
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-14 22:00:47 +1000

    add user context to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App.jsx";
+import { UserContext } from "./components/AccountContext.jsx";
+
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserContext>
+        <App />
+      </UserContext>
     </BrowserRouter>
   </React.StrictMode>
 );
