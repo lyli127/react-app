2fc36a8c1502eb2479d7e60825a3439a00b75021
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-14 16:02:23 +1000

    add functionality to login form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,57 @@
+//React
+import { React, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+//Bootstrap
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
+//My Components
 import { MainNav } from "./MainNav";
 import { Footer } from "./Footer";
+import { AccountContext } from "./AccountContext";
 
 function Login() {
+  const { setUser } = useContext(AccountContext);
+  const navigate = useNavigate();
+  const [fields, setFields] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleFieldChange(event) {
+    setFields({
+      ...fields,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // console.log(fields);
+    fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify(fields),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(...data);
+        navigate("/my-reviews");
+      })
+      .catch((error) => {
+        console.log(error);
+        if (response.status >= 400 && response.status < 600) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+      });
+  }
+
   return (
     <>
       <MainNav />
@@ -22,7 +66,7 @@ function Login() {
         </Row>
       </Container>
 
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group
           as={Row}
           className="mb-3 justify-content-md-center"
@@ -37,6 +81,8 @@ function Login() {
               <Form.Control
                 type="email"
                 placeholder="name@example.com"
+                onChange={handleFieldChange}
+                autoComplete="email"
                 required
               />
             </FloatingLabel>
@@ -50,7 +96,13 @@ function Login() {
         >
           <Col xs={6}>
             <FloatingLabel controlId="floatingPassword" label="Password">
-              <Form.Control type="password" placeholder="Password" required />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={handleFieldChange}
+                autoComplete="current-password"
+                required
+              />
               <Form.Text id="passwordHelpBlock" muted>
                 Your password must be 8-20 characters long, contain letters and
                 numbers, and must not contain spaces, special characters, or
