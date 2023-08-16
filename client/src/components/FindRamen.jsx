2ed0caf2bbcd9918ce1c0c10d110aa33d99324c2
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-16 14:06:01 +1000

    add find ramen component

--- client/src/components/FindRamen.jsx
+++ client/src/components/FindRamen.jsx
@@ -1,8 +1,19 @@
+import { MainNav } from "./MainNav";
+import { Footer } from "./Footer";
+
 function FindRamen() {
   return (
     <>
-      <h2>Find Ramen</h2>
-      <h3>This is the page I'll have Google Maps on</h3>
+      <MainNav />
+      <br />
+      <br />
+      <div style={{ textAlign: "center" }}>
+        <h2>Find Ramen</h2>
+        <h3>This is the page I'll have Google Maps on</h3>
+      </div>
+      <br />
+      <br />
+      <Footer />
     </>
   );
 }
