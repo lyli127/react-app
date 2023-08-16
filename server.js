a60f302fc78e4fbaeea536254385cd2a9f671011
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-16 14:03:08 +1000

    clean server file - update routes

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import session from "express-session";
-import User from "./controllers/usersController.js";
 import RamenReview from "./controllers/ramenController.js";
-import Session from "./controllers/sessionController.js";
+
 import "dotenv/config";
 
 const app = express();
-const Router = express.Router();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
@@ -20,34 +17,6 @@ const corsOptions = {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
-app.use(
-  session({
-    secret: process.env.SESSION_KEY,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-//USERS
-// GET
-app.get("/api/users/:id", User.getUserById);
-// POST
-app.post("/api/users", User.createUser);
-// PUT
-app.put("/api/users/:id", User.updateUser);
-// DELETE
-app.delete("/api/users/:id", User.deleteUser);
-
-//SESSIONS
-
-//LOGIN
-app.get("/api/login", Session.getLogin);
-app.post("/api/login", Session.postLogin);
-
-//LOGOUT
-
-//SIGNUP
-app.post("/signup", Session.postSignup);
 
 //RAMEN REVIEW
 // GET
@@ -57,10 +26,10 @@ app.get(
   "/api/:restaurant_name/reviews",
   RamenReview.getRamenReviewByRestaurant
 );
+app.get("/api/reviews/all", RamenReview.getAllRamenReviews);
 // POST
 app.post("/api/review", RamenReview.createRamenReview);
-// PUT
-app.put("/api/review/:id", RamenReview.updateRamenReview);
+
 // DELETE
 app.delete("/api/review/:id", RamenReview.deleteRamenReview);
 
