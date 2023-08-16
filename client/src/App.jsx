c7122ca2d46626fec0d267ebbdc39b2e02141261
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-16 14:04:47 +1000

    clean up file - remove unnecessary code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,66 +1,15 @@
 //REACT
 import "./App.css";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import { createContext, useContext, useMemo, useState } from "react";
 
 //MY COMPONENTS
-import { useLocalStorage } from "./utils/useLocalStorage";
 import Home from "./components/Home";
-import { Login } from "./components/Login";
-import SignUp from "./components/SignUp";
 import ViewAllReviewsPage from "./components/ViewAllReviewsPage";
-import EditReviewPage from "./components/EditReviewPage";
 import FindRamen from "./components/FindRamen";
 import ViewRestaurantReviews from "./components/ViewRestaurantReviews";
-// import { UserContext } from "./components/AccountContext";
 import { CreateReview } from "./components/CreateReview";
-import { AccountContext } from "./components/AccountContext";
-
-// const AuthContext = createContext();
-
-// function AuthProvider({ children }) {
-//   const [user, setUser] = useLocalStorage("user", null);
-
-//   const navigate = useNavigate();
-//   //call this function when you want to authenticate the user
-//   async function login(data) {
-//     setUser(data);
-//     navigate("/");
-//   }
-//   //call this function to sign out logged in user
-//   function logout() {
-//     setUser(null);
-//     navigate("/", { replace: true });
-//   }
-//   //memoization
-//   const value = useMemo(() => {
-//     user, login, logout;
-//   }, [user]);
-//   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-// }
-
-// function useAuth() {
-//   return useContext(AuthContext);
-// }
 
 function App() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const navigate = useNavigate();
-
-  // function handleAuthClick() {
-  //   if (isLoggedIn) {
-  //     // User is loggin out. Go to hompage.
-  //     navigate("/");
-  //   } else {
-  //     // User is logging in. Go to /my-reviews
-  //     navigate("/my-reviews");
-  //   }
-
-  //   setIsLoggedIn(() => !isLoggedIn);
-  // }
-
-  const { user } = useContext(AccountContext);
-
   return (
     <>
       <Routes>
@@ -81,6 +30,7 @@ function App() {
               path="/reviews/:restaurantName"
               element={<ViewRestaurantReviews />}
             />
+            <Route path="/restaurant" element={<ViewRestaurantReviews />} />
             {/* <Route element={<PrivateRoutes />}> */}
             <Route path="/review/edit/:id" element={<EditReviewPage />} />
             {/* </Route> */}
