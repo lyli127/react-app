1ddfbf39e9397cc76bd56260071f068eb9756f5c
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-17 22:42:17 +1000

    remove unused code

--- client/src/components/ViewAllReviewsPage.jsx
+++ client/src/components/ViewAllReviewsPage.jsx
@@ -1,6 +1,5 @@
 // React
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 //Custom Components
 import { Review } from "./Review";
@@ -8,27 +7,17 @@ import { MainNav } from "./MainNav";
 import { Footer } from "./Footer";
 
 //Bootstrap
-import Button from "react-bootstrap/Button";
 import CardGroup from "react-bootstrap/CardGroup";
 import Spinner from "react-bootstrap/Spinner";
 
-function ViewAllReviewsPage(props) {
+function ViewAllReviewsPage() {
   const [reviewList, setReviewList] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
-  const navigate = useNavigate();
-
-  function handleDelete(reviewId) {
-    // 1. Call your API to delete this review
-    // 2. If successful:
-    //    - Get the current `reviewList`
-    //    - Remove the review with the matching `reviewId`
-    //    - Use `setReviewList` to send the updated list
-  }
 
   //get a list of all reviews on page load => save as state then loop over the list and display each one
   useEffect(() => {
     if (!isLoaded) {
-      fetch(`http://localhost:3000/api/review/${props.user_id}/all`)
+      fetch(`http://localhost:3000/api/reviews/all`)
         .then((response) => response.json())
         .then((data) => {
           setReviewList(data);
@@ -47,20 +36,11 @@ function ViewAllReviewsPage(props) {
       <CardGroup>
         {isLoaded ? (
           reviewList.map((review) => (
-            <Review data={review} className="review-card" key={review.id}>
-              <div className="button" style={{ padding: "1%" }}>
-                <Button
-                  variant="primary"
-                  onClick={() => navigate(`/review/edit/${review.id}`)}
-                >
-                  Edit Review
-                </Button>
-                {"      "}
-                <Button variant="secondary" onClick={handleDelete(review.id)}>
-                  Delete
-                </Button>
-              </div>
-            </Review>
+            <Review
+              data={review}
+              className="review-card"
+              key={review.id}
+            ></Review>
           ))
         ) : (
           <>
