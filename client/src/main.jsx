c8e47a46654b0f14c3049c17ce9c8fca0226572f
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-17 15:36:03 +1000

    clean up main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App.jsx";
-import { UserContext } from "./components/AccountContext.jsx";
 
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,9 +9,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserContext>
-        <App />
-      </UserContext>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
