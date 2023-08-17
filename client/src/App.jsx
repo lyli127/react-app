c36e058fa1f89d21437a9ad9ffafc5cb7faf1a28
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-17 22:41:15 +1000

    routes refactor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 //REACT
 import "./App.css";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 //MY COMPONENTS
 import Home from "./components/Home";
-import ViewAllReviewsPage from "./components/ViewAllReviewsPage";
 import FindRamen from "./components/FindRamen";
 import ViewRestaurantReviews from "./components/ViewRestaurantReviews";
+import ViewAllReviewsPage from "./components/ViewAllReviewsPage";
 import { CreateReview } from "./components/CreateReview";
 
 function App() {
@@ -15,14 +15,11 @@ function App() {
       <Routes>
         <>
           <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/add-review" element={<CreateReview />} />
-          <Route path="/my-reviews" element={<ViewAllReviewsPage />} />
           <Route path="/find-ramen" element={<FindRamen />} />
-          <Route
-            path="/reviews/:restaurantName"
-            element={<ViewRestaurantReviews />}
-          />
-          <Route path="/restaurant" element={<ViewRestaurantReviews />} />
+          <Route path="/all-reviews" element={<ViewAllReviewsPage />} />
+          <Route path="/reviews/:slug" element={<ViewRestaurantReviews />} />
         </>
       </Routes>
     </>
