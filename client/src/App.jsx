5a4f70ac29aee1727bb900c36f0ac909d62502ab
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-17 15:32:59 +1000

    refact and clean app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,29 +13,17 @@ function App() {
   return (
     <>
       <Routes>
-        {user.loggedIn === null ? (
-          " "
-        ) : (
-          <>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/add-review" element={<CreateReview />} />
-            <Route
-              path="/my-reviews"
-              element={<ViewAllReviewsPage user_id="1" />}
-            />
-            <Route path="/find-ramen" element={<FindRamen />} />
-            <Route
-              path="/reviews/:restaurantName"
-              element={<ViewRestaurantReviews />}
-            />
-            <Route path="/restaurant" element={<ViewRestaurantReviews />} />
-            {/* <Route element={<PrivateRoutes />}> */}
-            <Route path="/review/edit/:id" element={<EditReviewPage />} />
-            {/* </Route> */}
-          </>
-        )}
+        <>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-review" element={<CreateReview />} />
+          <Route path="/my-reviews" element={<ViewAllReviewsPage />} />
+          <Route path="/find-ramen" element={<FindRamen />} />
+          <Route
+            path="/reviews/:restaurantName"
+            element={<ViewRestaurantReviews />}
+          />
+          <Route path="/restaurant" element={<ViewRestaurantReviews />} />
+        </>
       </Routes>
     </>
   );
