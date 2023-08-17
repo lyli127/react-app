ce7c12e771bcba40b7a93fb59d1ad58e8a5f96dd
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-17 15:40:40 +1000

    update ramen controlled to include rest table

--- controllers/ramenController.js
+++ controllers/ramenController.js
@@ -1,29 +1,37 @@
 import "dotenv/config";
+import slugify from "slugify";
 import pool from "../pool.js";
 
 //Create new ramen review
-const createRamenReview = (request, response) => {
-  const {
-    restaurant_name,
-    dish_name,
-    photo_url,
-    ramen_type,
-    ramen_score,
-    noodle_texture,
-    noodle_score,
-    broth_type,
-    broth_score,
-    chashu_type,
-    chashu_score,
-    ajitama,
-    ajitama_score,
-    other_notes,
-    date_visited,
-  } = request.body;
-  pool.query(
-    "INSERT INTO ramen (restaurant_name, dish_name, photo_url, ramen_type,ramen_score, noodle_texture, noodle_score, broth_type, broth_score, chashu_type, chashu_score, ajitama, ajitama_score, other_notes, date_visited) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *",
-    [
-      restaurant_name,
+const createRamenReview = async (request, response) => {
+  try {
+    let restaurantId;
+
+    // Step 1: Try to get the restaurant id via its slug.
+    restaurantId = (
+      await pool.query("SELECT id FROM restaurants WHERE slug = $1", [
+        slugify(request.body.restaurant_name),
+      ])
+    ).rows[0]?.id;
+
+    // Step 2: If restaurant doesn't exist, create new restaurant in restaurants table and get the id.
+    // Note: This will set the Restaurant name/slug to whatever the user has entered. We're getting it
+    //       later via a loose `ILIKE '%$1%'` query... so buyer beware.
+    if (!restaurantId) {
+      restaurantId = (
+        await pool.query(
+          "INSERT INTO restaurants (name, slug) VALUES ($1, $2) RETURNING id",
+          [
+            request.body.restaurant_name,
+            slugify(request.body.restaurant_name).toLowerCase(),
+          ]
+        )
+      ).rows[0]?.id;
+    }
+
+    // Step 3: Insert new ramen review into review table.
+    const {
+      // restaurant_name,
       dish_name,
       photo_url,
       ramen_type,
@@ -38,31 +46,36 @@ const createRamenReview = (request, response) => {
       ajitama_score,
       other_notes,
       date_visited,
-    ],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      response
-        .status(201)
-        .send(`Ramen review added with ID: ${results.rows[0].id}`);
-    }
-  );
-};
+    } = request.body;
 
-//Read all ramen review
-const getAllRamenReviewsByUser = (request, response) => {
-  const user_id = parseInt(request.params.user_id);
-  pool.query(
-    "SELECT * FROM ramen WHERE user_id= $1 ORDER BY id DESC",
-    [user_id],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      response.status(200).json(results.rows);
-    }
-  );
+    const dbResp = await pool.query(
+      "INSERT INTO reviews (restaurant_id, dish_name, photo_url, ramen_type,ramen_score, noodle_texture, noodle_score, broth_type, broth_score, chashu_type, chashu_score, ajitama, ajitama_score, other_notes, date_visited) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *",
+      [
+        restaurantId,
+        dish_name,
+        photo_url,
+        ramen_type,
+        ramen_score,
+        noodle_texture,
+        noodle_score,
+        broth_type,
+        broth_score,
+        chashu_type,
+        chashu_score,
+        ajitama,
+        ajitama_score,
+        other_notes,
+        date_visited,
+      ]
+    );
+
+    return response
+      .status(201)
+      .json(`Ramen review added with ID: ${dbResp.rows[0]?.id}`);
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json(error);
+  }
 };
 
 //Read ramen review by id
@@ -80,9 +93,30 @@ const getRamenReviewById = (request, response) => {
 //Read ramen review by restaurant id/name
 const getRamenReviewByRestaurant = (request, response) => {
   const restaurantName = request.params.restaurant_name;
+
   pool.query(
-    "SELECT * FROM ramen WHERE restaurant_name = $1",
-    [restaurantName],
+    `SELECT
+      reviews.id,
+      reviews.dish_name,
+      restaurants.name as restaurant_name,
+      restaurants.slug as restaurant_slug,
+      reviews.photo_url,
+      reviews.ramen_type,
+      reviews.ramen_score,
+      reviews.noodle_texture,
+      reviews.noodle_score,
+      reviews.broth_type,
+      reviews.broth_score,
+      reviews.chashu_type,
+      reviews.chashu_score,
+      reviews.ajitama,
+      reviews.ajitama_score,
+      reviews.other_notes,
+      reviews.date_visited
+    FROM reviews
+    JOIN restaurants ON reviews.restaurant_id = restaurants.id
+    WHERE restaurants.slug ILIKE $1`,
+    [`%${slugify(restaurantName)}%`],
     (error, results) => {
       if (error) {
         throw error;
@@ -108,57 +142,6 @@ const getAllRamenReviews = (request, response) => {
   );
 };
 
-//Update ramen review
-const updateRamenReview = (request, response) => {
-  const id = parseInt(request.params.id);
-  const {
-    restaurant_name,
-    dish_name,
-    photo_url,
-    ramen_type,
-    ramen_score,
-    noodle_texture,
-    noodle_score,
-    broth_type,
-    broth_score,
-    chashu_type,
-    chashu_score,
-    ajitama,
-    ajitama_score,
-    other_notes,
-    date_visited,
-    user_id,
-  } = request.body;
-  pool.query(
-    "UPDATE ramen SET restaurant_name=$1, dish_name=$2, photo_url=$3, ramen_type=$4, ramen_score=$5, noodle_texture=$6, noodle_score=$7, broth_type=$8, broth_score=$9, chashu_type=$10, chashu_score=$11, ajitama=$12, ajitama_score=$13, other_notes=$14, date_visited=$15, user_id=$16 WHERE id=$17",
-    [
-      restaurant_name,
-      dish_name,
-      photo_url,
-      ramen_type,
-      ramen_score,
-      noodle_texture,
-      noodle_score,
-      broth_type,
-      broth_score,
-      chashu_type,
-      chashu_score,
-      ajitama,
-      ajitama_score,
-      other_notes,
-      date_visited,
-      user_id,
-      id,
-    ],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      response.status(200).json(`Ramen review modified with ID: ${id}`);
-    }
-  );
-};
-
 //Delete ramen review
 const deleteRamenReview = (request, response) => {
   const id = parseInt(request.params.id);
@@ -175,10 +158,8 @@ const deleteRamenReview = (request, response) => {
 
 export default {
   createRamenReview,
-  getAllRamenReviewsByUser,
   getRamenReviewByRestaurant,
   getAllRamenReviews,
   getRamenReviewById,
-  updateRamenReview,
   deleteRamenReview,
 };
