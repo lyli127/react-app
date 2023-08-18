ae917dbf45dc13c35e058af29769b2ac0b3d4998
Lyli T <64179648+lyli127@users.noreply.github.com>
2023-08-18 14:37:33 +1000

    fix icon path

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,3 @@
-// import linkedin from "../assets/linkedin.png";
-// import github from "../assets/github.png";
-// import background from "../assets/background.svg";
 import Image from "react-bootstrap/Image";
 
 export function Footer() {
@@ -13,14 +10,11 @@ export function Footer() {
       <div className="container">
         <span className="text-muted">
           <br />
-          <img src="./favicon.ico" alt="ramen logo" width="100" />
+          <img src="/favicon.ico" alt="ramen logo" width="100" />
           <br />
           <p>
             <span className="text-muted">Lylibete Tennikoff © 2023</span>
           </p>
-          {/* <Image src={linkedin} alt="linkedin" />
-          <Image src={github} alt="github" /> */}
-          {/* <Image src="../assets/email" alt="email" /> */}
         </span>
       </div>
     </footer>
